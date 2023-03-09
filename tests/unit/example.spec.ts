4a277ae30f622c42d64fef14d1afc9e8eb512fc7
Advaita Kṛṣṇa Dāsa <advaita.krishna.das@gmail.com>
2023-03-09 09:55:37 +0100

    fix!

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,9 +1,9 @@
 import { mount } from '@vue/test-utils'
-import LibraryListPage from '@/library/pages/LibraryListPage.vue'
+import LogInPage from '@/auth/pages/LogInPage.vue'
 
 describe('LibraryListPage.vue', () => {
   it('renders tab 1 Tab1Page', () => {
-    const wrapper = mount(LibraryListPage)
-    expect(wrapper.text()).toMatch('Verses')
+    const wrapper = mount(LogInPage)
+    expect(wrapper.text()).toMatch('Log In')
   })
 })
