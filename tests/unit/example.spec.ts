f53647790b72b3bd96b58a541946f23acedbf897
Advaita Kṛṣṇa Dāsa <advaita.krishna.das@gmail.com>
2023-03-09 09:42:29 +0100

    fix tests

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,9 +1,9 @@
 import { mount } from '@vue/test-utils'
-import Tab1Page from '@/views/Tab1Page.vue'
+import LibraryListPage from '@/library/pages/LibraryListPage.vue'
 
-describe('Tab1Page.vue', () => {
+describe('LibraryListPage.vue', () => {
   it('renders tab 1 Tab1Page', () => {
-    const wrapper = mount(Tab1Page)
-    expect(wrapper.text()).toMatch('Tab 1 page')
+    const wrapper = mount(LibraryListPage)
+    expect(wrapper.text()).toMatch('Verses')
   })
 })
